0b5c8cb019fda02aae14889a95c245a589a0811d
Eminence <himanshumaurya447756@gmail.com>
2026-04-20 16:45:42 +0000

    log config

--- biddingengine/src/main/java/com/emi/biddingengine/BiddingengineApplication.java
+++ biddingengine/src/main/java/com/emi/biddingengine/BiddingengineApplication.java
@@ -1,7 +1,9 @@
 package com.emi.biddingengine;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BiddingengineApplication {
@@ -10,4 +12,10 @@ public class BiddingengineApplication {
 		SpringApplication.run(BiddingengineApplication.class, args);
 	}
 
+		@Bean
+    public CommandLineRunner printEnv() {
+        return args -> {
+            System.out.println("NEON_DATABASE_URL = " + System.getenv("NEON_DATABASE_URL"));
+        };
+    }
 }
